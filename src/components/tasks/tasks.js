4267fb4944e0225bf0c334c0f599002680c11edf
Husan Eshonqulov <husan.eshonqulov.00@gmail.com>
2022-07-08 22:32:37 +0500

    some styles

--- src/components/tasks/tasks.js
+++ src/components/tasks/tasks.js
@@ -1,10 +1,13 @@
 import createElement from "../element/element";
 import createTask from "../task/task";
+import './tasks.css';
 
 function createTasks(taskList) {
     const tasks = createElement('div');
 
     taskList.forEach(task => tasks.appendChild(createTask(task)));
+
+    tasks.classList.add('tasks');
 
     return tasks;
 }
